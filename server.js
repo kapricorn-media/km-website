b380a38cf33ff4f493e366e44ae769698ef1d657
Jose M Rico <jrico@princeton.edu>
2018-03-21 04:49:52 -0400

    Trying out react

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/react")));
 
 app.listen(port, function() {
     console.log("App listening on port " + port);
