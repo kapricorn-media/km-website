bd4e3d732bbcc160c93b298bf8f3f839e0814d9e
Jose M Rico <jrico@princeton.edu>
2019-01-12 13:33:39 -0500

    Added HTTP & HTTPS servers

--- server.js
+++ server.js
@@ -1,52 +1,71 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-const util = require("util");
 const bodyParser = require("body-parser");
 const cson = require("cson");
+const express = require("express");
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
+const path = require("path");
+const util = require("util");
 
+const PORT_HTTP = 8080;
+const PORT_HTTPS = 8181;
 const app = express();
-const port = process.env.PORT || 8080;
+
+const privateKey = fs.readFileSync("/etc/letsencrypt/live/kapricornmedia.com/privkey.pem", "utf8");
+const cert = fs.readFileSync("/etc/letsencrypt/live/kapricornmedia.com/cert.pem", "utf8");
+const ca = fs.readFileSync("/etc/letsencrypt/live/kapricornmedia.com/chain.pem", "utf8");
+
+const credentials = {
+	key: privateKey,
+	cert: cert,
+	ca: ca
+};
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "/public")));
 
 // Serve post entries
 app.post("/posts", function(req, res) {
-    var MAX_POSTS = 20;
+	var MAX_POSTS = 20;
 
-    var type = req.query.type;
-    if (typeof(type) !== "string") {
-        res.status(400);
-        res.send("Type argument invalid");
-        return;
-    }
+	var type = req.query.type;
+	if (typeof(type) !== "string") {
+		res.status(400);
+		res.send("Type argument invalid");
+		return;
+	}
 
-    var names = req.body;
-    if (!Array.isArray(names) || names.length > MAX_POSTS) {
-        res.status(400);
-        res.send("Names not an array or larger than "
-            + MAX_POSTS + " elements");
-        return;
-    }
+	var names = req.body;
+	if (!Array.isArray(names) || names.length > MAX_POSTS) {
+		res.status(400);
+		res.send("Names not an array or larger than "
+			+ MAX_POSTS + " elements");
+		return;
+	}
 
-    var result = [];
-    for (var i = 0; i < names.length; i++) {
-        var postFilePath = path.join(__dirname, "/posts", type,
-            names[i] + ".cson");
-        try {
-            var postData = cson.parse(fs.readFileSync(postFilePath));
-        } catch (e) {
-            res.status(400);
-            res.send("Requested non-existent file: " + postFilePath);
-            return;
-        }
-        result.push(postData);
-    }
+	var result = [];
+	for (var i = 0; i < names.length; i++) {
+		var postFilePath = path.join(__dirname, "/posts", type,
+			names[i] + ".cson");
+		try {
+			var postData = cson.parse(fs.readFileSync(postFilePath));
+		} catch (e) {
+			res.status(400);
+			res.send("Requested non-existent file: " + postFilePath);
+			return;
+		}
+		result.push(postData);
+	}
 
-    res.send(result);
+	res.send(result);
 });
 
-app.listen(port, function() {
-    console.log("App listening on port " + port);
+const httpServer = http.createServer(app);
+httpServer.listen(PORT_HTTP, function() {
+	console.log("HTTP server listening on port " + PORT_HTTP);
+});
+
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(PORT_HTTPS, function() {
+	console.log("HTTPS server listening on port " + PORT_HTTPS);
 });
